eb7e63f8e60d0d04dd114ac2f5c34f1289633004
jcadelacruz <112672810+jcadelacruz@users.noreply.github.com>
2025-05-08 23:54:57 +0800

    minor : components.js component color themes
    1. residential (1) purple
    2. rest (7) green
    3. admin (4) orange
    4. sports (2) red
    5. academic (3) blue

--- components.js
+++ components.js
@@ -163,13 +163,15 @@ function createClipPath(vs){
     return x;
 }
 
+
 function initComponents(window){
     window.components = {};
     components = window.components;
     var title = new Component("Title", "title","0%","0%","50%","50%", "0", "", "", "", 1);
     //title.panelDetails.disabled = true;
     title.panelDetails.htmlComponent.innerHTML = "The plan is focused on creating a harmonious environment that fosters collaboration, innovation, and a deep connection to nature, inviting students and faculty alike to thrive in a vibrant and inspiring academic oasis.";
-    var dorms = new Component("DORM", "dorm", "21.5%", "37%", "43%", "8.5%" , "0", "", "This is breindel yes", "#8f00ff");
+//1.RESIDENTIAL ZONE (1)
+    var dorms = new Component("DORM", "dorm", "21.5%", "37%", "43%", "8.5%" , "0", "", "This is breindel yes", "#574964");
     let vs = [
     0,0,
     13.7,0,
@@ -179,9 +181,11 @@ function initComponents(window){
     0,15.4
     ];
     dorms.mapComponent.style.clipPath = createClipPath(vs);
-    var kala = new Component("KALA LANE", "kala", "4%", "17%", "61.3%", "40.5%", "0", "", "smol", "#FFFFC5");
-    var rest = new Component("REST AREA", "rest", "7.8%", "12.8%", "55.8%", "27%" , "0", "", "hrm chem bio", "pink");
-    var forest = new Component("FOREST", "forest" , "10.5%", "63%", "89%", "2%" , "0", "", "This is breindel yes", "#06402B", 1.5);
+
+//2.REST ZONE (7)
+    var kala = new Component("KALA LANE", "kala", "4%", "17%", "61.3%", "40.5%", "0", "", "smol", "#F0F0D7");
+    var rest = new Component("REST AREA", "rest", "7.8%", "12.8%", "55.8%", "27%" , "0", "", "hrm chem bio", "#D0DDD0");
+    var forest = new Component("FOREST", "forest" , "10.5%", "63%", "89%", "2%" , "0", "", "This is breindel yes", "#727D73", 1.5);
 
     vs = [
     0,0.5,
@@ -195,11 +199,13 @@ function initComponents(window){
     ];
     forest.mapComponent.style.clipPath = createClipPath(vs);
 
-    var cafeteria = new Component("CAFETERIA", "cafeteria", "20%", "16.5%", "53.63%", "58.5%", "0", "", "smol", "#40ff8c");
+    var cafeteria = new Component("CAFETERIA", "cafeteria", "20%", "16.5%", "53.63%", "58.5%", "0", "", "smol", "#BAD8B6");
     cafeteria.mapComponent.style.borderRadius = "100000px";
-    var grandstand = new Component("GRANDSTAND", "grandstand", "4.4%", "3.8%", "51.8%", "52.3%", "0", "", "smol", "#C04000");
-    var admin = new Component("ADMIN", "admin", "12.7%", "10.4%", "84.7%", "62.8%", "3", "", "smol", "black");
-    var dropoff = new Component("DROPOFF", "dropoff", "20%", "16.5%", "48.75%", "77%", "0", "", "smol", "#8f9779");
+    var garden = new Component("GARDEN", "garden", "5.1%", "4.2%", "73.8%", "52%" , "0", "", "hrm chem bio", "#AAB99A");
+    var grandstand = new Component("GRANDSTAND", "grandstand", "4.4%", "3.8%", "51.8%", "52.3%", "0", "", "smol", "#E1EACD");
+//3.ADMIN ZONE (4)
+    var admin = new Component("ADMIN", "admin", "12.7%", "10.4%", "84.7%", "62.8%", "3", "", "smol", "#FFB38E");
+    var dropoff = new Component("DROPOFF", "dropoff", "20%", "16.5%", "48.75%", "77%", "0", "", "smol", "#FFCF9D");
     vs = [
     0,0,
     5.5,0.2,
@@ -209,12 +215,13 @@ function initComponents(window){
     0,4.15
     ];
     dropoff.mapComponent.style.clipPath = createClipPath(vs);
-    var flag = new Component("FLAGPOLE", "flag", "11.2%", "5.8%", "67.5%", "78.7%", "-1", "", "smol", "white");
+    var flag = new Component("FLAGPOLE", "flag", "11.2%", "5.8%", "67.5%", "78.7%", "-1", "", "smol", "#FFB26F");
     flag.mapComponent.style.backgroundImage = "url('https://img.freepik.com/free-vector/illustration-philippinesflag_53876-27126.jpg')";
     flag.mapComponent.style.backgroundRepeat = "no-repeat";
     flag.mapComponent.style.backgroundSize = "100% 100%";
-    var wait = new Component("WAIT", "wait", "4.8%", "3.6%", "70.25%", "88.5%", "0", "", "smol", "#722F37");
+    var wait = new Component("WAIT", "wait", "4.8%", "3.6%", "70.25%", "88.5%", "0", "", "smol", "#DE8F5F");
 
+//4.SPORTS ZONE (2)
     var gym = new Component("GYM", "gym", "50%", "34%", "5%" , "47.86%", "11.6", "", "hrm chem bio", "#ff4040", 1.7);
     vs = [
     4,0,
@@ -228,8 +235,9 @@ function initComponents(window){
     var field = new Component("FIELD", "field", "27%", "20.3%", "16.15%" , "60.55%", "12", "", "hrm chem bio", "#ff2060", 1.7);
     field.mapComponent.style.borderRadius = "2.5vw";
 
-    var classrms = new Component("CLASSRMS", "classrms", "11%", "34%", "67.65%", "18%" , "0", "", "hrm chem bio", "lightblue");
-    var acadAttach = new Component("XACAD", "acadAttach", "26%", "20%", "67.6%", "5.5%" , "0", "", "hrm chem bio", "blue");
+//5.ACADEMIC ZONE (3)
+    var classrms = new Component("CLASSRMS", "classrms", "11%", "34%", "67.65%", "18%" , "0", "", "hrm chem bio", "#9EC6F3");
+    var acadAttach = new Component("XACAD", "acadAttach", "26%", "20%", "67.6%", "5.5%" , "0", "", "hrm chem bio", "#C6E7FF");
     vs = [
     0,0,
     16.7,0,
@@ -239,7 +247,6 @@ function initComponents(window){
     0,6.2
     ];
     acadAttach.mapComponent.style.clipPath = createClipPath(vs);
-    var labs = new Component("LABORATORIES", "labs", "10%", "27.5%", "83.4%", "24.5%" , "0", "", "hrm chem bio", "royalblue");
+    var labs = new Component("LABORATORIES", "labs", "10%", "27.5%", "83.4%", "24.5%" , "0", "", "hrm chem bio", "#9FB3DF");
     var pond = new Component("POND", "pond", "3.7%", "9%", "79.3%", "33%" , "0", "", "hrm chem bio", "orange");
-    var garden = new Component("GARDEN", "garden", "5.1%", "4.2%", "73.8%", "52%" , "0", "", "hrm chem bio", "yellow");
 }
