43dcc6a3ed49d2799f137c5d0a1da006b94df36a
Sarj2026 <143585791+Sarj2026@users.noreply.github.com>
2025-05-09 13:41:29 +0800

    html links attribute "redir" + instances update

--- components.js
+++ components.js
@@ -2,13 +2,13 @@ let components = {};
 
 let clicked;
 class Component{
-    constructor(n, varName, w,h,l,t,r, i,d,c,z=2){
+    constructor(n, varName, w,h,l,t,r, i,d,c, redir, z=2){
         allComps.push(this);
     
         this.name = n;
         this.color = c;
         
-        this.panelComponent = new PanelComponent(this,n,i,d,c);
+        this.panelComponent = new PanelComponent(this,n,i,d,c, redir);
         this.panelDetails = this.panelComponent.panelDetails;
         
         let mc = document.createElement('div');
@@ -86,7 +86,7 @@ class Component{
     }
 }
 class PanelComponent{
-    constructor(t,n,i,d,c){
+    constructor(t,n,i,d,c, redir){
         this.name = n;
         this.color = c;
         this.initialColor = i;
@@ -104,7 +104,7 @@ class PanelComponent{
         pc.addEventListener("click", t.handleClick.bind(t));
         this.htmlComponent = pc;
         
-        this.panelDetails = new PanelDetails(this,d,i,c, "images/panelDtl"+n+".img");
+        this.panelDetails = new PanelDetails(this,d,i,c, "images/panelDtl"+n+".img", redir);
     }
     highlight(isReversed){
         if(isReversed){
@@ -116,7 +116,7 @@ class PanelComponent{
     }
 }
 class PanelDetails{
-    constructor(pc,d,i,c, imag){
+    constructor(pc,d,i,c, imag,redir){
         this.disabled = false; 
         
         let pd = document.createElement('div');
@@ -135,8 +135,12 @@ class PanelDetails{
         this.htmlComponent.appendChild(t);
         
         let l = document.createElement('a');
-        l.href = pc.name+'.html';
+        l.href = redir;
         l.textContent = 'See Details [>]';
+        console.log(this,redir);
+        if(redir===""){
+            l.style.display = "none";
+        }
         this.htmlHyperlink = l;
         this.htmlComponent.appendChild(l);
     }
@@ -177,7 +181,7 @@ function initComponents(window){
     title.panelDetails.htmlComponent.innerHTML = "The plan is focused on creating a harmonious environment that fosters collaboration, innovation, and a deep connection to nature, inviting students and faculty alike to thrive in a vibrant and inspiring academic oasis.";
     const purple="#B7B1F2",green="#D5E5D5",orange="#FFF1D5",red="#F6D6D6",blue="#D4F6FF";
 //1.RESIDENTIAL ZONE (1)
-    var dorms = new Component("DORM", "dorm", "21.5%", "37%", "43%", "8.5%" , "0", "rgb(221 196 245)", "This is breindel yes", "#574964");
+    var dorms = new Component("DORM", "dorm", "21.5%", "37%", "43%", "8.5%" , "0", "rgb(221 196 245)", "This is breindel yes", "#574964", "dormsZone.html");
     let vs = [
     0,0,
     13.7,0,
@@ -189,9 +193,9 @@ function initComponents(window){
     dorms.mapComponent.style.clipPath = createClipPath(vs);
 
 //2.ACADEMIC ZONE (3)
-    var classrms = new Component("CLASSRMS", "classrms", "11%", "34%", "67.65%", "18%" , "0", blue, "hrm chem bio", "#9EC6F3");
-    var labs = new Component("LABORATORIES", "labs", "10%", "27.5%", "83.4%", "24.5%" , "0", blue, "hrm chem bio", "#9FB3DF");
-    var acadAttach = new Component("XACAD", "acadAttach", "26%", "20%", "67.6%", "5.5%" , "0", blue, "hrm chem bio", "#C6E7FF");
+    var classrms = new Component("CLASSRMS", "classrms", "11%", "34%", "67.65%", "18%" , "0", blue, "hrm chem bio", "#9EC6F3", "academicZone.html");
+    var labs = new Component("LABORATORIES", "labs", "10%", "27.5%", "83.4%", "24.5%" , "0", blue, "hrm chem bio", "#9FB3DF", "academicZone.html");
+    var acadAttach = new Component("XACAD", "acadAttach", "26%", "20%", "67.6%", "5.5%" , "0", blue, "hrm chem bio", "#C6E7FF", "academicZone.html");
     vs = [
     0,0,
     16.7,0,
@@ -204,11 +208,11 @@ function initComponents(window){
     var pond = new Component("POND", "pond", "3.7%", "9%", "79.3%", "33%" , "0", green, "hrm chem bio", "orange");
 
 //3.REST ZONE (7)
-    var cafeteria = new Component("CAFETERIA", "cafeteria", "20%", "16.5%", "53.63%", "58.5%", "0", green, "smol", "#BAD8B6");
+    var cafeteria = new Component("CAFETERIA", "cafeteria", "20%", "16.5%", "53.63%", "58.5%", "0", green, "smol", "#BAD8B6", "restZone.html");
     cafeteria.mapComponent.style.borderRadius = "100000px";
-    var rest = new Component("REST AREA", "rest", "7.8%", "12.8%", "55.8%", "27%" , "0", green, "hrm chem bio", "#AAB99A");
+    var rest = new Component("REST AREA", "rest", "7.8%", "12.8%", "55.8%", "27%" , "0", green, "hrm chem bio", "#AAB99A", "restZone.html");
     
-    var forest = new Component("FOREST", "forest" , "10.5%", "63%", "89%", "2%" , "0", green, "This is breindel yes", "#727D73", 1.5);
+    var forest = new Component("FOREST", "forest" , "10.5%", "63%", "89%", "2%" , "0", green, "This is breindel yes", "#727D73", "restZone.html", 1.5);
 
     vs = [
     0,0.5,
@@ -222,17 +226,17 @@ function initComponents(window){
     ];
     forest.mapComponent.style.clipPath = createClipPath(vs);
 
-    var kala = new Component("KALA LANE", "kala", "4%", "17%", "61.3%", "40.5%", "0", green, "smol", "#F0F0D7");
-    var grandstand = new Component("GRANDSTAND", "grandstand", "4.4%", "3.8%", "51.8%", "52.3%", "0", green, "smol", "#E1EACD");
-    var garden = new Component("GARDEN", "garden", "5.1%", "4.2%", "73.8%", "52%" , "0", green, "hrm chem bio", "#D0DDD0");
+    var kala = new Component("KALA LANE", "kala", "4%", "17%", "61.3%", "40.5%", "0", green, "smol", "#F0F0D7", "restZone.html");
+    var grandstand = new Component("GRANDSTAND", "grandstand", "4.4%", "3.8%", "51.8%", "52.3%", "0", green, "smol", "#E1EACD", "restZone.html");
+    var garden = new Component("GARDEN", "garden", "5.1%", "4.2%", "73.8%", "52%" , "0", green, "hrm chem bio", "#D0DDD0", "restZone.html");
 
 //4.ADMIN ZONE (4)
-    var admin = new Component("ADMIN", "admin", "12.7%", "10.4%", "84.7%", "62.8%", "3", orange, "smol", "#FFB38E");
-    var flag = new Component("FLAGPOLE", "flag", "11.2%", "5.8%", "67.5%", "78.7%", "-1", orange, "smol", "#FFB26F");
+    var admin = new Component("ADMIN", "admin", "12.7%", "10.4%", "84.7%", "62.8%", "3", orange, "smol", "#FFB38E", "");
+    var flag = new Component("FLAGPOLE", "flag", "11.2%", "5.8%", "67.5%", "78.7%", "-1", orange, "smol", "#FFB26F","");
     flag.mapComponent.style.backgroundImage = "url('https://img.freepik.com/free-vector/illustration-philippinesflag_53876-27126.jpg')";
     flag.mapComponent.style.backgroundRepeat = "no-repeat";
     flag.mapComponent.style.backgroundSize = "100% 100%";
-    var dropoff = new Component("DROPOFF", "dropoff", "20%", "16.5%", "48.75%", "77%", "0", orange, "smol", "#FFCF9D");
+    var dropoff = new Component("DROPOFF", "dropoff", "20%", "16.5%", "48.75%", "77%", "0", orange, "smol", "#FFCF9D","");
     vs = [
     0,0,
     5.5,0.2,
@@ -242,10 +246,10 @@ function initComponents(window){
     0,4.15
     ];
     dropoff.mapComponent.style.clipPath = createClipPath(vs);
-    var wait = new Component("WAIT", "wait", "4.8%", "3.6%", "70.25%", "88.5%", "0", orange, "smol", "#DE8F5F");
+    var wait = new Component("WAIT", "wait", "4.8%", "3.6%", "70.25%", "88.5%", "0", orange, "smol", "#DE8F5F","");
 
 //5.SPORTS ZONE (2)
-    var gym = new Component("GYM", "gym", "50%", "34%", "5%" , "47.86%", "11.6", red, "hrm chem bio", "#DC8686", 1.7);
+    var gym = new Component("GYM", "gym", "50%", "34%", "5%" , "47.86%", "11.6", red, "hrm chem bio", "#DC8686","sportsZone.html", 1.7);
     vs = [
     4,0,
     26.65,0,
@@ -255,7 +259,7 @@ function initComponents(window){
     0.7,13.7
     ];
     gym.mapComponent.style.clipPath = createClipPath(vs);
-    var field = new Component("FIELD", "field", "27%", "20.3%", "16.15%" , "60.55%", "12", red, "hrm chem bio", "#D37676", 1.7);
+    var field = new Component("FIELD", "field", "27%", "20.3%", "16.15%" , "60.55%", "12", red, "hrm chem bio", "#D37676","sportsZone.html", 1.7);
     field.mapComponent.style.borderRadius = "2.5vw";
 
 }
